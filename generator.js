f5c832df3068ae56a74a2bd9e964418ea1ba9136
kamiokan <spherenov@gmail.com>
2021-05-14 12:05:01 +0900

    2.4.1 ジェネレータの生成

---
+++ generator.js
@@ -0,0 +1,11 @@
+function* generatorFunc() {
+  console.log("ジェネレータ関数開始");
+  console.log("yield 1");
+  yield 1;
+  console.log("yield 2");
+  yield 2;
+  console.log("yield 3");
+  yield 3;
+  console.log("ジェネレータ関数終了");
+  return "ジェネレータ関数戻り値";
+}
